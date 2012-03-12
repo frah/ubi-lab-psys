92cc7ff9934a8a4053303dd65e60b867813cc71d
Atsushi OHNO <atsushi-o@is.naist.jp>
2012-03-12 16:45:18 +0900

    FirstCommit

---
+++ src/org/ubilab/payment/UbiLabPaymentSystem.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.ubilab.payment;
+
+/**
+ *
+ * @author atsushi-o
+ */
+public class UbiLabPaymentSystem {
+    public UbiLabPaymentSystem() {
+        new mainWindow().setVisible(true);
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new UbiLabPaymentSystem();
+    }
+}
